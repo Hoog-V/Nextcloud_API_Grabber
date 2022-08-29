42e60a8dffc873d38b63c30ffa2be6021f6505a9
Hoog-V <hogeweyv@gmail.com>
2022-08-29 06:34:11 +0200

    -add skeleton upload example

--- examples/upload_file/upload_file.c
+++ examples/upload_file/upload_file.c
@@ -1,3 +1,12 @@
-//
-// Created by victor on 29-8-22.
-//
+#include <api_grabber.h>
+
+#define SERVER_FILEPATH "Photos/Birdie.jpg" //Replace spaces with %20
+#define UPLOAD_FILEPATH "Birdie.jpg"
+
+#define NEXTCLOUD_BASE_URL "https://cloud.example.com"
+#define NEXTCLOUD_AUTH "USER:PASSWORD"
+#define NEXTCLOUD_USER "USER"
+
+int main(int argc, char *argv[]){
+    return 0;
+}
